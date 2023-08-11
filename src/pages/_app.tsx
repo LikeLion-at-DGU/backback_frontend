57a9faaff91593b49fe68ebc8981652d1e7ff6a6
tkdwns414 <sangjune2000@naver.com>
2023-08-12 00:10:14 +0900

    change title & favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import BaseLayout from '@/layouts/BaseLayout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <BaseLayout>
+      <Head>
+        <title>울끈불끈</title>
+        <link rel="icon" href="/favicon.png" />
+        <meta name="description" content="당신을 위한 내 손 안의 운동장" />
+      </Head>
       <Component {...pageProps} />
     </BaseLayout>
   )
