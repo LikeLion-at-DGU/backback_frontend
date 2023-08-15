cf7045a982227a8da2f47938754cd025f8d8a84b
tkdwns414 <sangjune2000@naver.com>
2023-08-16 05:57:24 +0900

    regist gymApi

--- src/apis/gymApi.ts
+++ src/apis/gymApi.ts
@@ -1,19 +1,23 @@
 import api from "@/api";
 
-interface GymsQueryParams {
-  address?: string;
-  location?: string;
-}
-
 const gymApi = () => {
   return {
-    getGyms: (query: GymsQueryParams) => {
-      const queryParams: GymsQueryParams = {};
+    getGyms: (query: { address: string }) => {
+      const queryParams: { address?: string } = {};
       if (query.address) queryParams.address = query.address;
-      if (query.location) queryParams.location = query.location;
       return api.get("/gyms", { params: queryParams });
     },
+    getGymsBylocation: (data: { latitude: number; longitude: number }) =>
+      api.post(`/gyms/use-location`, data),
     getGym: (id: number) => api.get(`/gyms/${id}`),
+    reportGym: (id: number, data: { reason: string }) =>
+      api.post(`/gyms/${id}/report`, data),
+    getGymReviews: (id: number) => api.get(`/gyms/${id}/reviews`),
+    postGymReview: (id: number, data: { content: string; key: string }) =>
+      api.post(`/gyms/${id}/reviews`, data),
+    deleteReview: (id: number) => api.delete(`/reviews/${id}`),
+    reportReview: (id: number, data: { reason: string }) =>
+      api.post(`/reviews/${id}/report`, data),
   };
 };
 
