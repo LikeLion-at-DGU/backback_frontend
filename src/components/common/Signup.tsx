06b321a3b7bda9cdca93eebd5960231b5b5aa454
0seoYun <yys020819@gmail.com>
2023-08-15 16:38:28 +0900

    fix/12 : url, margin 수정

--- src/components/common/Signup.tsx
+++ src/components/common/Signup.tsx
@@ -5,17 +5,17 @@ const Signup = () => {
 
     return (
         <div className={styles.center}>
-            <a href="http://localhost:4000/auth/kakao" >
+            <a href="http://localhost:3000/auth/kakao" >
                 <img
                     src="../../../assets/images/kakaologin.png"
                     style={{ height: "50px", width: "250px" }}
                     alt="kakaologin"
                 />
             </a>
-            <a href="http://localhost:4000/auth/google">
+            <a href="http://localhost:3000/auth/google">
                 <img
                     src="../../../assets/images/googlelogin.png"
-                    style={{ height: "50px", width: "250px", marginTop: "10px" }}
+                    style={{ height: "50px", width: "250px", marginTop: "15px" }}
                     alt="googlelogin"
                 />
             </a>
