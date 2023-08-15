bd25f45c6dbb465b163cba4e603d1c4e8a365f07
tkdwns414 <sangjune2000@naver.com>
2023-08-16 06:05:20 +0900

    regist profileApi

--- src/apis/profileApi.ts
+++ src/apis/profileApi.ts
@@ -1,7 +1,18 @@
-import api from '@/api';
+import api from "@/api";
 
 const profileApi = () => {
-    return {}
+  return {
+    getMe: () => api.get("/me"),
+    patchMe: (data: { nickname: string; intro: string }) =>
+      api.patch("/me", data),
+    getProfile: (id: number) => api.get(`/profile/${id}`),
+    getProfilePosts: (id: number) => api.get(`/profile/${id}/posts`),
+    getProfileCompletions: (id: number) =>
+      api.get(`/profile/${id}/completions`),
+    followProfile: (id: number) => api.post(`/profile/${id}/follow`, {}),
+    reportProfile: (id: number, data: { reason: string }) =>
+      api.post(`/profile/${id}/report`, { reason: data.reason }),
+  };
 };
 
 export default profileApi;
