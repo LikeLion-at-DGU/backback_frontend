c468ae4f62b54de759e5eda0b15463ad09a088ef
jihyunniiii <jita1125@naver.com>
2023-08-16 07:59:54 +0900

    feature/31: 마이페이지 오운완 POOL 날짜 변경 드롭박스 구현

--- src/components/mypage/CompletedPool.tsx
+++ src/components/mypage/CompletedPool.tsx
@@ -1,70 +1,48 @@
-import { Pool } from "./pool"
+import { DateDropdown } from "./DateDropdown";
+import { Pool } from "./pool";
 
 interface CompletedPoolProps {
-    joinDate: string,
-    completedList: number[]
+  joinDate: string;
+  completedList: number[];
 }
 
 const completedPoolTitleTextstyle = {
-    fontWeight: 700,
-    fontSize: '16px'
-}
+  fontWeight: 700,
+  fontSize: "16px",
+};
 
-const comletedPoolDateTextStyle = {
-    fontWeight: 500,
-    fontSize: '12px',
-    marginLeft: 'auto',
-    marginRight: '5px'
-}
+const comletedPoolDropdownStyle = {
+  marginLeft: "auto",
+  marginRight: "5px",
+};
 
-const completedPoolImageStyle = {
-    width: '14px',
-    height: '8px',
-    marginRight: '34px'
-}
-
-export function CompletedPool({joinDate, completedList}: CompletedPoolProps) {
-    const categotyUnderIconPath = '/assets/images/Category_Under_icon.png'
-    const joinDateForm = new Date(joinDate);
-    const joinYear = joinDateForm.getFullYear();
-    const joinMonth = joinDateForm.getMonth() + 1;
-    
-    return(
-        <div>
-            <div
-                style={{
-                    display: 'flex',
-                    margin: '12px 0px 0px 26px',
-                    alignItems: "center"
-                }}
-            >
-                <p
-                    style={completedPoolTitleTextstyle}
-                >
-                    오운완 POOL
-                </p>
-                <p
-                    style={comletedPoolDateTextStyle}
-                >
-                    {joinYear}년 {joinMonth}월
-                </p>
-                <img
-                    src={categotyUnderIconPath}
-                    style={completedPoolImageStyle}
-                />
-            </div>
-            <div
-                style={{
-                    overflowX: 'auto'
-                }}
-            >
-                <Pool completedList={completedList}/>
-            </div>
-            <hr
-                style={{
-                    margin: '0 15px'
-                }}
-            ></hr>
+export function CompletedPool({ joinDate, completedList }: CompletedPoolProps) {
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          margin: "12px 0px 0px 26px",
+          alignItems: "center",
+        }}
+      >
+        <p style={completedPoolTitleTextstyle}>오운완 POOL</p>
+        <div style={comletedPoolDropdownStyle}>
+          <DateDropdown joinDate={joinDate} />
         </div>
-    )
+      </div>
+      <div
+        style={{
+          overflowX: "auto",
+        }}
+      >
+        <Pool completedList={completedList} />
+      </div>
+      <hr
+        style={{
+          margin: "0 15px",
+        }}
+      ></hr>
+    </div>
+  );
 }
