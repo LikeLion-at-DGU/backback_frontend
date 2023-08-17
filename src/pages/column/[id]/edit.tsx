a405cbafeeaab6bd67c6072b7ea825ef0719657f
0seoYun <yys020819@gmail.com>
2023-08-18 07:11:57 +0900

    feat/83 patchColumn

---
+++ src/pages/column/[id]/edit.tsx
@@ -0,0 +1,125 @@
+import { useRouter } from "next/router";
+import React, { useState, useEffect, ChangeEvent } from "react";
+import InputTitle from "@/components/write/InputTitle";
+import InputContent from "@/components/write/InputContent";
+import postApi from "@/apis/postApi";
+import { ScrollContent } from "@/components/post/PostDetail";
+import RouterLink from "@/components/core/RouterLink";
+
+export async function getServerSideProps(context: any) {
+  const params = context.params;
+  const id = params.id;
+
+  return { props: { id } };
+}
+
+export default function EditColumn(props: { id: string }) {
+  const router = useRouter();
+  const [selectedImages, setSelectedImages] = useState<FileList | null>(null);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [previewImageCount, setPreviewImageCount] = useState(0);
+
+  useEffect(() => {
+    postApi()
+      .getPost(props.id)
+      .then((res: any) => {
+        setTitle(res.data.title);
+        setContent(res.data.content);
+        setPreviewImageCount(res.data.images.length);
+      });
+  }, []);
+  const handleTitleChange = (event: any) => {
+    setTitle(event.target.value);
+  };
+  const handleContentChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(event.target.value);
+  };
+  const handleImageChange = (files: FileList | null) => {
+    setSelectedImages(files);
+    setPreviewImageCount(files?.length || 0);
+  };
+
+  const handleSubmit = () => {
+    if (title && content) {
+      const data = {
+        title: title,
+        content: content,
+        purpose: 0,
+        exercise: 0,
+        type: "PRO",
+      };
+      postApi()
+        .patchPost(props.id, data)
+        .then(() => (window.location.href = `/column/${props.id}`));
+    }
+  };
+
+  return (
+    <ScrollContent>
+      <div
+        style={{
+          width: "100%",
+          height: "50px",
+          padding: "0px 15px 0px 15px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-end",
+          fontSize: "16px",
+          borderBottom: "1px solid #B7BBC8",
+        }}
+      >
+        <button
+          onClick={() => {
+            handleSubmit();
+          }}
+          style={{
+            border: "none",
+            backgroundColor: "transparent",
+            fontFamily: "MainFont",
+            fontSize: "16px",
+            cursor: "pointer",
+            marginRight: "10px",
+          }}
+        >
+          수정
+        </button>
+      </div>
+      <div
+        style={{
+          width: "100%",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "15px",
+          flex: "1",
+          overflow: "auto",
+          scrollBehavior: "smooth",
+          display: "flex",
+        }}
+      >
+        <div style={{ flex: "1" }}>
+          <div
+            style={{
+              width: "100%",
+              justifyContent: "center",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              flex: "1",
+            }}
+          >
+            <div>
+              <InputTitle title={title} onTitleChange={handleTitleChange} />
+            </div>
+            <div style={{ marginTop: "20px" }}>
+              <InputContent
+                content={content}
+                onContentChange={handleContentChange}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </ScrollContent>
+  );
+}
